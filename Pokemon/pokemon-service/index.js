a915f0cc71d491c7c3e3732e9a7cc626d80dfa2b
Felix <taylo4ag@dukes.jmu.edu>
2021-10-19 14:42:50 -0400

    changed get

--- Pokemon/pokemon-service/index.js
+++ Pokemon/pokemon-service/index.js
@@ -8,101 +8,95 @@ const connection = mysql.createConnection(credentials);
 const service = express();
 service.use(express.json());
 
-connection.connect (error => {
-  if (error) {
-   console.error(error);
-   process.exit(1);
-  }
-  });
-
-
-  const port = 5001;
-  service.listen(port, () => {
-    console.log(`We're live in port ${port}!`);
-  });
-
-function rowToMon(row) {
-  return {
-    id: row.id,
-    name: row.name,
-    sprite: row.sprite,
-    typeOne: row.typeOne,
-    typeTwo: row.typeTwo,
-    description: row.description,
-    genus: row.genus,
-  };
-  }
-service.get('pokemon/:name', (request, response) => {
-  const query = 'SELECT * FROM mon WHERE name = ' + request.params.name;
-  connection.query(query, (error, rows) => {
+connection.connect(error => {
     if (error) {
-      response.status(500);
-      response.json({
-        ok:false,
-        results: "Pokemon does not exist yet.",
-      })
-    } else {
-      response.json({
-        ok:true,
-        results: rows.map(rowToMon)
-      });
+        console.error(error);
+        process.exit(1);
     }
-  });
+});
+
+const port = 5001;
+service.listen(port, () => {
+    console.log(`We're live in port ${port}!`);
 });
 
 async function getNameSpriteType(name) {
-  try {
-  var response = await fetch('https://pokeapi.co/api/v2/pokemon/' + name);
-  var data = response.json();
-  var result;
-  result.name = response.name;
-  result.sprite = response.sprites.front_default;
-  result.typeOne = response.types[0].type.name;
-  result.typeTwo = NULL;
-  if (response.types.length > 1) {
-    result.typeTwo = response.types[1].type.name;
-  }
-  return result;
-  }
-  catch (err) {
-    alert(err);
-    return NULL;
-  }
+    try {
+        var response = await fetch('https://pokeapi.co/api/v2/pokemon/' + name);
+        var data = await response.json();
+        var result;
+        result.name = data.name;
+        result.sprite = data.sprites.front_default;
+        result.typeOne = data.types[0].type.name;
+        result.typeTwo = NULL;
+        if (data.types.length > 1) {
+            result.typeTwo = data.types[1].type.name;
+        }
+        return result;
+    } catch (err) {
+        alert(err);
+        return NULL;
+    }
 }
 
 async function getIdDescGenus(name) {
-  try {
-  var response = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + name);
-  var data = await response.json();
-  var info;
-  for (let i = 0; i < data.pokedex_numbers.length; i++) {
-    let entry = data.pokedex_numbers[i];
-    if (entry[i].pokedex.name === "national") {
-      info.id = parseInt(entry.entry_number);
-      break;
+    try {
+        var response = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + name);
+        var data = await response.json();
+        var info;
+        for (let i = 0; i < data.pokedex_numbers.length; i++) {
+            let entry = data.pokedex_numbers[i];
+            if (entry[i].pokedex.name === "national") {
+                info.id = parseInt(entry.entry_number);
+                break;
+            }
+        }
+        for (let i = 0; i < data.flavor_text_entries.length; i++) {
+            let entry = data.flavor_text_entries[i];
+            if (entry.language.name === "en") {
+                info.description = entry.flavor_text;
+                break;
+            }
+        }
+        for (let i = 0; i < data.genera.length; i++) {
+            let entry = data.genera[i];
+            if (entry.language.name === "en") {
+                info.genus = entry.genus;
+                break;
+            }
+        }
+        return info;
+    } catch (err) {
+        alert(err);
+        return NULL;
     }
-  }
-  for (let i = 0; i < data.flavor_text_entries.length; i++) {
-    let entry = data.flavor_text_entries[i];
-    if (entry.language.name === "en") {
-      info.description = entry.flavor_text;
-      break;
-    }
-  }
-  for (let i = 0; i < data.genera.length; i++) {
-    let entry = data.genera[i];
-    if (entry.language.name === "en") {
-      info.genus = entry.genus;
-      break;
-    }
-  }
-  return info;
-} catch (err) {
-  alert(err);
-  return NULL;
-}
 }
 
+service.get('/pokemon/:name', (request, response) => {
+    var name = request.params.name.toLowerCase();
+    var nameSpriteType = getnameSpriteType(name);
+    var idDescGenus = getIdDescGenus(name);
+    if (nameSpriteType == null || idDescGenus == null) {
+        response.json({
+            ok: false,
+            results: `Pokemon could not be found: ${name}`,
+        })
+    } else {
+        var result;
+        result.id = idDescGenus.id
+        result.name = nameSpriteType.name;
+        result.typeOne = nameSpriteType.typeOne;
+        result.typeTwo = nameSpriteType.typeTwo;
+        result.genus = idDescGenus.genus;
+        result.description = idDescGenus.description;
+        response.json({
+            ok: true,
+            results: result,
+        });
+    }
+});
+
+/**
 service.post('pokemon/:name', (request, response) => {
   if (service.get(request.params.name).ok) {
     response.json({
@@ -145,3 +139,27 @@ service.post('pokemon/:name', (request, response) => {
       }
     });
 });
+*/
+
+
+/**
+service.get('pokemon/:name', (request, response) => {
+
+
+  /**
+  const query = 'SELECT * FROM mon WHERE name = ' + request.params.name;
+  connection.query(query, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok:false,
+        results: "Pokemon does not exist yet.",
+      })
+    } else {
+      response.json({
+        ok:true,
+        results: rows.map(rowToMon)
+      });
+    }
+  });
+});*/
