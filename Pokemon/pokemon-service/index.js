6b44c394edb458191b209cc0ed07b39b7878acd1
Felix <taylo4ag@dukes.jmu.edu>
2021-10-25 13:34:10 -0400

    ?

--- Pokemon/pokemon-service/index.js
+++ Pokemon/pokemon-service/index.js
@@ -17,7 +17,7 @@ connection.connect(error => {
     }
 });
 
-connection.query("INSERT INTO mon VALUES (54, psyduck, 0)");
+connection.query("INSERT INTO mon VALUES (54, 'psyduck', 0)");
 connection.query("INSERT INTO mon VALUES (55, golduck, 0)");
 connection.query("INSERT INTO mon VALUES (56, mankey, 0)");
 connection.query("INSERT INTO mon VALUES (57, primeape, 0)");
