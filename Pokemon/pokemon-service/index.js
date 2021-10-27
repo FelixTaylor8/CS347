060a01507675d503026545418e79edde20b6c9dd
Felix <taylo4ag@dukes.jmu.edu>
2021-10-27 12:40:07 -0400

    get report

--- Pokemon/pokemon-service/index.js
+++ Pokemon/pokemon-service/index.js
@@ -28,7 +28,7 @@ service.options('*', (request, response) => {
 });
 
 service.get('/report.html', (request, response) => {
-  response.sendFile('report.html');
+  response.sendFile('/home/apir/CS347/Pokemon/pokemon-services/report.html');
 });
 
 /**
